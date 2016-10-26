d9185b4ecc9482ea4404526e95d390771300d2e2
Neil Johnson <neil@andalsosmokeypo.com>
2016-10-26 16:36:19 +1100

    add emojis index pat and rows

--- Emoji Dictionary/ViewController.swift
+++ Emoji Dictionary/ViewController.swift
@@ -8,11 +8,32 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var dacooltableview: UITableView!
+    
+    
+    var emojis = ["🍏","🍒","🍉","🎩","🐏","😀","🌻","⚽️","😀","🐟","🐝","🌵","🍄"]
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        dacooltableview.dataSource = self
+        dacooltableview.delegate = self
+    }
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return emojis.count
+        
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = emojis[indexPath.row]
+        return cell
     }
 
     override func didReceiveMemoryWarning() {
