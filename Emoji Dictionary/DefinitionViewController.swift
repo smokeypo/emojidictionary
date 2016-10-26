c867d383ab2f4c490df151423d6c72bded8b4729
Neil Johnson <neil@andalsosmokeypo.com>
2016-10-26 18:12:38 +1100

    definition view finder emoji picker

---
+++ Emoji Dictionary/DefinitionViewController.swift
@@ -0,0 +1,32 @@
+//
+//  DefinitionViewController.swift
+//  Emoji Dictionary
+//
+//  Created by Neil Johnson on 26/10/16.
+//  Copyright © 2016 Smokeypo. All rights reserved.
+//
+
+import UIKit
+
+class DefinitionViewController: UIViewController {
+    
+    @IBOutlet weak var emojiLabel: UILabel!
+    var emoji = "NO EMOJI"
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+       emojiLabel.text = emoji
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+
+
+}
--- Emoji Dictionary/ViewController.swift
+++ Emoji Dictionary/ViewController.swift
@@ -37,7 +37,14 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-      performSegue(withIdentifier: "moveSegue", sender: "🍏")
+        let emoji = emojis[indexPath.row]
+      performSegue(withIdentifier: "moveSegue", sender: emoji)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+      let defVC = segue.destination as!
+        DefinitionViewController
+        defVC.emoji = sender as! String
     }
 
     override func didReceiveMemoryWarning() {
